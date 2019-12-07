d1d4448e10d168d9074aba97379d618c48a663f8
aacgn <4augusto@gmail.com>
2019-12-07 19:21:56 -0300

    Create pipefy clone application

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 0px 30px;
+  background: #7159c1;
+  color: #FFFFFF;
+
+  display: flex;
+  align-items: center;
+`;
